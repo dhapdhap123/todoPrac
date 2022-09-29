4228a7d1a9c213d20f7e4c458bc3282638608c48
dhapdhap123 <dhapdhap123@naver.com>
2022-09-29 23:28:40 +0900

    0929

--- src/componenets/TodoInsert.js
+++ src/componenets/TodoInsert.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        onChange={onChange}
+        value={value}
+      />
       <button type="submit">
         <MdAdd />
       </button>
